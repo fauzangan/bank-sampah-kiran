96df27e32f5c1427aa62e3b2f39bccfef7bdfdfe
Fauzan Zaman <63907675+fauzangan@users.noreply.github.com>
2023-10-26 08:43:51 +0700

    Fix seeder and route

--- resources/views/dashboard/penimbangan/penarikan.blade.php
+++ resources/views/dashboard/penimbangan/penarikan.blade.php
@@ -1,5 +1,29 @@
 @extends('dashboard.layouts.main')
 @section('content')
+{{-- Symbol --}}
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+        <path
+            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+    </symbol>
+</svg>
+{{-- Symbol --}}
+{{-- Alert --}}
+@if(session()->has('errors'))
+@foreach ($errors->all() as $error)
+<div class="alert alert-danger alert-dismissible fade show d-flex align-items-center" role="alert">
+    <svg class="bi flex-shrink-0" width="24" height="24" role="img" aria-label="Success:">
+        <use xlink:href="#exclamation-triangle-fill" />
+    </svg>
+    <div class="mx-2">
+        {{ $error }}
+    </div>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endforeach
+@endif
+{{-- Alert --}}
+
 <div class="card">
     <div class="card-body">
         <h4 class="card-title">Penarikan</h4>
@@ -50,8 +74,8 @@
                     <div class="col-md-auto">
                     </div>
                 </div>
-
             </div>
+            
             <div class="form-group">
                 <label for="">Nasabah</label>
                 <div class="row">
@@ -105,50 +129,6 @@
         </div>
     </div>
 </div>
-
-<!-- responsive kolom jenis sampah dan kolom pilih nasabah-->
-<script>
-    function jenisResponsive(elementId, defaultWidth, maxWidth) {
-        const element = document.getElementById(elementId);
-
-        function adjustWidth() {
-            const screenWidth = window.innerWidth || document.documentElement.clientWidth;
-            const newWidth = Math.min(maxWidth, screenWidth);
-
-            element.style.width = (newWidth / maxWidth) * 100 + '%';
-        }
-
-        // Initial adjustment
-        adjustWidth();
-
-        // Adjust width when the window is resized
-        window.addEventListener('resize', adjustWidth);
-        }
-
-        // Call the function to make the element with ID "pilih" responsive
-        jenisResponsive('select-jenis-sampah', 1000, 1200);
-
-    function userResponsive(elementId, defaultWidth, maxWidth) {
-        const element = document.getElementById(elementId);
-
-        function adjustWidth() {
-            const screenWidth = window.innerWidth || document.documentElement.clientWidth;
-            const newWidth = Math.min(maxWidth, screenWidth);
-
-            element.style.width = (newWidth / maxWidth) * 100 + '%';
-        }
-
-        // Initial adjustment
-        adjustWidth();
-
-        // Adjust width when the window is resized
-        window.addEventListener('resize', adjustWidth);
-        }
-
-        // Call the function to make the element with ID "pilih" responsive
-        userResponsive('select-user', 1000, 1200);
-
-</script>
 
 <script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
