67442832109295389c1964cc1b505d9dea927d19
Fauzan Zaman <63907675+fauzangan@users.noreply.github.com>
2023-11-06 22:42:46 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function() {
-    return redirect()->route('test');
+    return redirect()->route('login');
 });
 
 Route::get('/read-loadcell', [LoadcellController::class, 'readWeight']);
